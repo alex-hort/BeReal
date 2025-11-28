c53f665cd894781176a441ae1fe9e909f394839c
“alex-hort” <ahortealese001@alumno.uaemex.mx>
2025-11-28 17:58:34 -0600

    Initial commit

---
+++ BeReal/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  BeReal
+//
+//  Created by Alexis Horteales Espinosa on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeReal/Feed.swift
@@ -0,0 +1,18 @@
+//
+//  Feed.swift
+//  BeReal
+//
+//  Created by Alexis Horteales Espinosa on 28/11/25.
+//
+
+import SwiftUI
+
+struct Feed: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Feed()
+}
